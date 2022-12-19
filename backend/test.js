6028e650e916260390655cc23020b4cb4531d7fc
chavanrutvik50 <55613870+chavanrutvik50@users.noreply.github.com>
2022-12-19 13:50:53 +0530

    Delete test.js

--- backend/test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function getDirectories(path) {
-    return fs.readdirSync(path).filter(function (file) {
-        return fs.statSync(path + '/' + file);
-    });
-}
-
-console.log(getDirectories("./storage"));
